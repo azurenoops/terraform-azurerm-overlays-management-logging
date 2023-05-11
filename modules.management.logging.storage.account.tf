3da49316e337932e1e472a37b8842b63fc5b5414
John Spinella <jrspinella@users.noreply.github.com>
2023-05-11 09:14:37 -0400

    Updates to SA

--- modules.management.logging.storage.account.tf
+++ modules.management.logging.storage.account.tf
@@ -9,15 +9,15 @@ module "loganalytics_sa" {
   version                      = ">= 0.1.0"
   depends_on                   = [module.mod_scaffold_rg]
   existing_resource_group_name = local.resource_group_name
-  storage_account_custom_name  = local.spoke_sa_name
+  org_name                     = var.org_name
   location                     = local.location
   environment                  = var.environment
   deploy_environment           = var.deploy_environment
   workload_name                = "${var.workload_name}${random_id.uniqueString.hex}"
-  org_name                     = var.org_name
-  account_kind                 = var.loganalytics_storage_account_kind
-  account_tier                 = var.loganalytics_storage_account_tier
-  account_replication_type     = var.loganalytics_storage_account_replication_type
+  # Storage Account
+  account_kind             = var.loganalytics_storage_account_kind
+  account_tier             = var.loganalytics_storage_account_tier
+  account_replication_type = var.loganalytics_storage_account_replication_type
   # Locks
   enable_resource_locks = var.enable_resource_locks
   add_tags              = merge(local.default_tags, var.add_tags, )
